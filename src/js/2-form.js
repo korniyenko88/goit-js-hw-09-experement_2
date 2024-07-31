01d5070362a9c45e0b3449caa29a6a6cd62a9989
unknown <you@example.com>
2024-07-31 12:01:47 +0300

    update task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,5 +1,5 @@
 const createForm = () => {
-    return `
+  return `
     <form class="feedback-form" autocomplete="off">
         <label>
             Email
@@ -15,20 +15,37 @@ const createForm = () => {
 
 const formAdd = document.querySelector('.task-form');
 formAdd.innerHTML = createForm();
-
+const formData = { email: '', message: '' };
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const email = form.email.value;
-    const message = form.message.value;
+const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-    console.log('Email:', email);
-    console.log('Message:', message);
-    console.log(window.localStorage);
+if (savedData) {
+  formData.email = savedData.email || '';
+  formData.message = savedData.message || '';
+  form.email.value = formData.email;
+  form.message.value = formData.message;
+}
 
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  formData[name] = value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
 
+form.addEventListener('submit', event => {
+  event.preventDefault();
 
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
 
-   
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
+  form.email.value = '';
+  form.message.value = '';
 });
