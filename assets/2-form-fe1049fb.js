6f5760ba5caccdf029e153e711e2a13788eac679
korniyenko88 <165329483+korniyenko88@users.noreply.github.com>
2024-08-01 09:21:25 +0000

    Deploying to gh-pages from @ korniyenko88/goit-js-hw-09@130794b4fc8f9302284840b4aa9a8b06056b2704 🚀

---
+++ assets/2-form-fe1049fb.js
@@ -0,0 +1,13 @@
+const o=()=>`
+    <form class="feedback-form" autocomplete="off">
+        <label>
+            Email
+            <input type="email" name="email" autofocus />
+        </label>
+        <label>
+            Message
+            <textarea name="message" rows="8"></textarea>
+        </label>
+        <button type="submit">Submit</button>
+    </form>`,r=document.querySelector(".task-form");r.innerHTML=o();const e={email:"",message:""},a=document.querySelector(".feedback-form"),m=JSON.parse(localStorage.getItem("feedback-form-state"));m&&(e.email=m.email||"",e.message=m.message||"",a.email.value=e.email,a.message.value=e.message);a.addEventListener("input",t=>{const{name:s,value:l}=t.target;e[s]=l.trim(),localStorage.setItem("feedback-form-state",JSON.stringify(e))});a.addEventListener("submit",t=>{if(t.preventDefault(),!e.email||!e.message){alert("Fill please all fields");return}console.log(e),localStorage.removeItem("feedback-form-state"),e.email="",e.message="",a.email.value="",a.message.value=""});
+//# sourceMappingURL=2-form-fe1049fb.js.map
