be78d39bff6f9aa8be801bcb0e262e8c9dcf164f
korniyenko88 <165329483+korniyenko88@users.noreply.github.com>
2024-08-05 10:12:25 +0000

    Deploying to gh-pages from @ korniyenko88/goit-js-hw-09@953032708b8335d6a07d20e36b261c6eca729580 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,13 +1,2 @@
-import"./assets/styles-f0e51143.js";const o=()=>`
-    <form class="feedback-form" autocomplete="off">
-        <label>
-            Email
-            <input type="email" name="email" autofocus />
-        </label>
-        <label>
-            Message
-            <textarea name="message" rows="8"></textarea>
-        </label>
-        <button type="submit">Submit</button>
-    </form>`,r=document.querySelector(".task-form");r.innerHTML=o();const e={email:"",message:""},a=document.querySelector(".feedback-form"),m=JSON.parse(localStorage.getItem("feedback-form-state"));m&&(e.email=m.email||"",e.message=m.message||"",a.email.value=e.email,a.message.value=e.message);a.addEventListener("input",t=>{const{name:s,value:l}=t.target;e[s]=l.trim(),localStorage.setItem("feedback-form-state",JSON.stringify(e))});a.addEventListener("submit",t=>{if(t.preventDefault(),!e.email||!e.message){alert("Fill please all fields");return}console.log(e),localStorage.removeItem("feedback-form-state"),e.email="",e.message="",a.email.value="",a.message.value=""});
+import"./assets/styles-f0e51143.js";const e={email:"",message:""},a=document.querySelector(".feedback-form"),s=JSON.parse(localStorage.getItem("feedback-form-state"));s&&(e.email=s.email||"",e.message=s.message||"",a.email.value=e.email,a.message.value=e.message);a.addEventListener("input",t=>{const{name:m,value:l}=t.target;e[m]=l.trim(),localStorage.setItem("feedback-form-state",JSON.stringify(e))});a.addEventListener("submit",t=>{if(t.preventDefault(),!e.email||!e.message){alert("Fill please all fields");return}console.log(e),localStorage.removeItem("feedback-form-state"),e.email="",e.message="",a.email.value="",a.message.value=""});
 //# sourceMappingURL=commonHelpers2.js.map
