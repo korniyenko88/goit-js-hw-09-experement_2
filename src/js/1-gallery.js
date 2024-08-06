e00d43ecd82723e62c67e9da0a612322975f31ea
unknown <you@example.com>
2024-08-06 12:51:55 +0300

    update

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,9 @@
+
+
 import SimpleLightbox from "simplelightbox";
+
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 const images = [
   {
---
+++ src/simplelightbox.d.ts
@@ -0,0 +1,6 @@
+declare module 'simplelightbox' {
+    export default class SimpleLightbox {
+      constructor(element: string | HTMLElement, options?: any);
+      
+    }
+  }
